598a635620f5f8da729d2c8a7d3f11bfc41e5e35
sasha <57253679+AlexGrom1999@users.noreply.github.com>
2022-03-04 21:54:59 +0200

    started project

---
+++ explosion-kittens/Assets/Scripts/Game.cs
@@ -0,0 +1,27 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviourPunCallbacks
+{
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Leave()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(newPlayer.NickName + "connected");
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.Log(otherPlayer.NickName + "disconnect");
+    }
+}
---
+++ explosion-kittens/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,48 @@
+using System;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+
+    [SerializeField] private TextMeshProUGUI logText;
+    private void Awake()
+    {
+        PhotonNetwork.NickName = "Player" + Random.Range(0, 1000);
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = "1";
+        Log(PhotonNetwork.NickName);
+        
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Log("connected to master");
+    }
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(null, new RoomOptions {MaxPlayers = 2});
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Log("join room");
+        PhotonNetwork.LoadLevel("Lobby 1");
+    }
+
+    public void Log(string log)
+    {
+        Debug.Log(log);
+        logText.text += log;
+    }
+}

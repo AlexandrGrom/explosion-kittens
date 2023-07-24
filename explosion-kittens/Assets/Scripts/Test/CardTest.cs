4f3249cf674b12b9cc396646fd231a71b5c79a1f
AlexandrGrom <68745248+AlexandrGrom@users.noreply.github.com>
2023-07-24 16:40:48 +0300

    test polish

--- explosion-kittens/Assets/Scripts/Test/CardTest.cs
+++ explosion-kittens/Assets/Scripts/Test/CardTest.cs
@@ -1,24 +1,90 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class CardTest : MonoBehaviour
+public class CardTest : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler, IBeginDragHandler
 {
     [SerializeField] private Transform _target;
+    [SerializeField] private RectTransform localTarget;
     [SerializeField] private int speed = 10;
+    [SerializeField] private LayoutElement _element ;
+    private Vector3 targetPos;
     public Vector3 offset = Vector3.zero;
+    public Vector3 rotation;
+    private Canvas _canvas;
+    private bool drag;
+    private int targetIndex;
+    private Transform _world;
+
     
-    public void SetTarget(Transform target)
+
+    
+
+    public void SetTarget(Transform target, Canvas canvas, Transform world)
     {
+        targetIndex = transform.GetSiblingIndex();
+        
+        var localTargetGo = new GameObject();
+        localTarget = localTargetGo.AddComponent<RectTransform>();
+        localTargetGo.transform.SetParent(world);
+        Canvas.ForceUpdateCanvases();
+        localTargetGo.transform.position = target.position;
+        localTargetGo.transform.localScale = Vector3.zero;
+
         _target = target;
+        _canvas = canvas;
     }
 
     private void Update()
     {
-        if (_target != null)
+        //if (_target == null) return;
+
+        if (drag)
         {
-            transform.position = Vector3.Lerp(transform.position, _target.position + offset, Time.deltaTime * speed);
+            transform.position = Vector3.Lerp(transform.position, localTarget.position, Time.deltaTime * speed * 2) ;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, Time.deltaTime * speed);
+
         }
+        else
+        {
+            localTarget.transform.position = _target.position;
+
+            transform.position = Vector3.Lerp(transform.position, localTarget.position + offset, Time.deltaTime * speed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(rotation), Time.deltaTime * speed);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.SetSiblingIndex(transform.parent.childCount);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        localTarget.anchoredPosition += eventData.delta / _canvas.scaleFactor;
+        
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, Time.deltaTime * speed);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        drag = false;
+        transform.SetSiblingIndex(targetIndex);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        drag = true;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            _canvas.transform as RectTransform,
+            Input.mousePosition, _canvas.worldCamera,
+            out var movePos);
+
+        
+        localTarget.position = _canvas.transform.TransformPoint(movePos);
+
+        //localTarget.anchoredPosition = eventData.position;
     }
 }
--- explosion-kittens/Assets/Scripts/Test/Foo.cs
+++ explosion-kittens/Assets/Scripts/Test/Foo.cs
@@ -1,57 +1,71 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class Foo : MonoBehaviour
+namespace Test
 {
-    [SerializeField] private Transform[] positions;
-    [SerializeField] private Transform start;
-    [SerializeField] private CardTest testPrefab;
-    [SerializeField] private List<CardTest> tests;
-
-    private void Awake()
+    public class Foo : MonoBehaviour
     {
-        tests = new List<CardTest>();
-        for (int i = 0; i < positions.Length; i++)
+        [SerializeField] private Transform[] positions;
+        [SerializeField] private Transform start;
+        [SerializeField] private CardTest testPrefab;
+        [SerializeField] private List<CardTest> tests;
+        [SerializeField] private AnimationCurve curve;
+        [SerializeField] private Canvas canvas;
+        [SerializeField] private Transform parent;
+        [SerializeField] private Transform world;
+
+        private void Awake()
         {
-            positions[i].gameObject.SetActive(false);
+            tests = new List<CardTest>();
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i].gameObject.SetActive(false);
+            }
         }
-    }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+        private void Update()
         {
-            if (tests.Count >= positions.Length) return;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (tests.Count >= positions.Length) return;
                 
-            var v = Instantiate(testPrefab, start.position, start.rotation, start.parent);
-            tests.Add(v);
-            var v1 = positions[tests.Count-1];
-            v1.gameObject.SetActive(true);
-            v.SetTarget(v1);
+                var v = Instantiate(testPrefab, parent);
+                v.transform.position = start.position;
+                tests.Add(v);
+                var v1 = positions[tests.Count-1];
+                v1.gameObject.SetActive(true);
+                v.SetTarget(v1, canvas, world);
 
             
-            float f = tests.Count - 1;
-            float f1 = positions.Length - 1;
-            float step;
-            if (f == 0)
-            {
-                step = 0;
-            }
-            else
-            {
-                step = f1 / f;
-            }
-            for (int i = 0; i < tests.Count; i++)
-            {
-                
-                float targetRot = 10 - step * i;
+                float f = tests.Count - 1;
+                float f1 = positions.Length - 1;
+                float step;
                 if (f == 0)
                 {
-                    targetRot = 0;
+                    step = 0;
                 }
-                tests[i].transform.localEulerAngles = new Vector3(0,0,targetRot);
+                else
+                {
+                    step = f1 / f;
+                }
+                for (int i = 0; i < tests.Count; i++)
+                {
+                
+                    float targetRot = 10 - step * i;
+                    if (f == 0)
+                    {
+                        targetRot = 0;
+                    }
+
+                    float position = (10 - targetRot) / 20;
+                
+                    tests[i].transform.localEulerAngles = new Vector3(0,0,targetRot);
+                    tests[i].rotation = new Vector3(0,0,targetRot);
+                    
+                    
+                    tests[i].offset = new Vector3(0,curve.Evaluate(position),0);
+                }
             }
         }
     }
